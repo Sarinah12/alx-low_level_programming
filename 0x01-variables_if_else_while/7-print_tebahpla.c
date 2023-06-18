fc6c4e3cab971f5e7ede27267a929a10f1f4aa01
Sarinah12 <makhado.sm@gmail.com>
2023-06-18 14:37:59 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,11 +5,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{char ch;
+{
+	char ch;
+
 	for (ch = 'z'; ch >= 'a'; ch--)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
-	return (0)
+	return (0);
 }
