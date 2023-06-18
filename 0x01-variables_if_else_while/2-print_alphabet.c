cf588919651707f74fff3e039c064451d74c747e
Sarinah12 <makhado.sm@gmail.com>
2023-06-18 09:53:02 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
- char alp[26] = "abcdefghijklmnopqrstuvwxyz";
- int i;
- for (i = 0; i < 26; i++)
- {
- putchar(alp[i]);
- }
- putchar('\n');
-2023/06/16, 18:22
-Page 2 of 8
- putchar('\n');
- return (0);
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i <26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
+
