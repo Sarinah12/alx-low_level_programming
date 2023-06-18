b74c5fd957e844d7aefdc7e14b4b81d09138080b
Sarinah12 <makhado.sm@gmail.com>
2023-06-18 13:29:21 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,14 @@
  */
 int main(void)
 {
+	int i;
+
 	for (i = 97; i < 123; i++)
 	{
-		if (i != 101 && !=113)
-		{ putchar(i);
+		if (i != 101 && i !=113)
+		{ 
+			putchar(i);
+
 		}
 	}
 	putchar('\n');
